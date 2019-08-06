ff8466ed0849711a467f4552576a8f3197d65650
Tiago Miotto <tiago.miotto@hotmail.com>
2019-08-06 17:57:25 +0100

    Implemented timescaleDB queries

--- timescaleSetup.sql
+++ timescaleSetup.sql
@@ -11,35 +11,3 @@ SELECT create_hypertable('motor', 'timestamp', chunk_time_interval => 3600000);
 select set_chunk_time_interval('accelerometer', 3600000);
 select set_chunk_time_interval('gyroscope', 3600000);
 select set_chunk_time_interval('motor', 3600000);
-
-elect time_bucket(300,timestamp) as five_seconds, avg(x) from gyroscope
-where timestamp between 1565105247177 and 1565105247478 group by five_seconds
-order by five_seconds desc;
-
-explain analyze with RequestedTimeSeries as (select generate_series(1565005247478 ,1565105247478, 300) Serie)
-            select RequestedTimeSeries.Serie as timestamp,
-            MAX(name) as name,  AVG(x) AS x, AVG(y) AS y, AVG(z) AS z from RequestedTimeSeries
-            left join gyroscope on gyroscope.timestamp >= RequestedTimeSeries.Serie
-            and gyroscope.timestamp< RequestedTimeSeries.Serie + 300 group by RequestedTimeSeries.Serie order by RequestedTimeSeries.Serie;
-
-
-explain analyze select time_bucket(300,timestamp) as timestamp, AVG(x) AS x, AVG(y) AS y, AVG(z) AS z from gyroscope
-where timestamp between 0 and 1565105247478 group by timestamp
-order by timestamp desc;
-
-
-with aux as(select time_bucket(300,timestamp) as interval, AVG(x) AS x, AVG(y) AS y, AVG(z) AS z
- from gyroscope g
-where timestamp between 1561005247478 and 1565105347478 group by interval
-order by interval desc)
-select interval as timestamp, x,y,z from aux;
-
-
-select * from gyroscope where timestamp between 1561005247478 and 1565105347478 order by timestamp desc limit 5;
-
-
-"WITH aggregation as" +
-            "(select time_bucket(:interval ,timestamp) as interval, AVG(x) AS x, AVG(y) AS y, AVG(z) AS z from #{#entityName} " +
-            "where timestamp between :begin and :end group by interval " +
-            "order by timestamp desc)" +
-            "SELECT interval as timestamp, x,y,z from aggregation",
